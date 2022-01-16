fde8249aa82dd4e226e0caf5a741f86c90a62898
gferro90 <wergrest@hotmail.it>
2022-01-16 21:42:34 +0100

    minor changes

--- MARTe/prepare.sh
+++ MARTe/prepare.sh
@@ -4,7 +4,7 @@
 	ls $PROJECT_DIR/ToolConfiguration/STM32ExtractFiles.py || cp $SCRIPT_DIR/STM32ExtractFiles.py $PROJECT_DIR/ToolConfiguration
 	ls stm32f4xx.h || cd $PROJECT_DIR/ToolConfiguration; python3 STM32ExtractFiles.py $PROJECT_NAME $PROJECT_NAME . ; cd -
 	ls $PROJECT_DIR/ToolConfiguration/HandleDatabase.* || cp $SCRIPT_DIR/HandleDatabase.* $PROJECT_DIR/ToolConfiguration
-	ls $PROJECT_DIR/ToolConfiguration/patch.sh || cp $SCRIPT_DIR/patch.sh $PROJECT_DIR/ToolConfiguration
+	ls $PROJECT_DIR/ToolConfiguration/patch.sh || cp $SCRIPT_DIR/STM32F4/patch.sh $PROJECT_DIR/ToolConfiguration
 	if [ -f $PROJECT_DIR/ToolConfiguration/usbd_cdc_if.c ]; then cp $PROJECT_DIR/ToolConfiguration/usbd_cdc_if_template_c $PROJECT_DIR/ToolConfiguration/usbd_cdc_if.c; fi
 	if [ -f $PROJECT_DIR/ToolConfiguration/usbd_cdc_if.h ]; then cp $PROJECT_DIR/ToolConfiguration/usbd_cdc_if_template_h $PROJECT_DIR/ToolConfiguration/usbd_cdc_if.h; fi	
 	if [ ! -f $PROJECT_DIR/ToolConfiguration/usbd_cdc.h ]; then find . -name usbd_cdc.h | xargs -I found_file cp found_file $PROJECT_DIR/ToolConfiguration/ ; fi
